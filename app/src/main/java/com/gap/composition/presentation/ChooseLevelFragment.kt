41fd7404a0b58c33718722d0f829857c87b10766
sergeygap <1ggserongg1@gmail.com>
2023-07-04 22:27:14 +0300

    add ViewBinding in all Fragment

--- app/src/main/java/com/gap/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/gap/composition/presentation/ChooseLevelFragment.kt
@@ -6,20 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.gap.composition.R
+import com.gap.composition.databinding.FragmentChooseLevelFragmentBinding
+import com.gap.composition.databinding.FragmentGameFinishedBinding
+import java.lang.RuntimeException
 
 class ChooseLevelFragment: Fragment() {
-
+    private var _binding: FragmentChooseLevelFragmentBinding? = null
+    private val binding: FragmentChooseLevelFragmentBinding
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelFragmentBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level_fragment, container, false)
+    ): View {
+        _binding = FragmentChooseLevelFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
