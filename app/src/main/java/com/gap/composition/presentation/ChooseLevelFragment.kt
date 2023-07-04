62146016198726e0af93837632ccfcbef74fbf13
sergeygap <1ggserongg1@gmail.com>
2023-07-04 21:52:09 +0300

    release UI and add res

---
+++ app/src/main/java/com/gap/composition/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,25 @@
+package com.gap.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gap.composition.R
+
+class ChooseLevelFragment: Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_choose_level_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/gap/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/gap/composition/presentation/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.gap.composition.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.gap.composition.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.main_container, WelcomeFragment())
+            .addToBackStack(null).commit()
     }
+
+
 }
